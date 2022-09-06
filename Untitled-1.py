b5f8b7e669afc0efa4c4778ee7fc548fd2d6c80d
Ciclo Básico - Micro 26 <gustavor272@gmail.com>
2022-09-06 14:21:36 -0300

    b

--- Untitled-1.py
+++ Untitled-1.py
@@ -2,3 +2,11 @@ def falaAlgo(x):
     print("falei",x)
 
 print(falaAlgo('luiz eh chato'))
+
+#Mas como add isso no GitHub?
+#git add .
+#git commit -m "Alterações"
+#git push .
+
+#Caso 2 pessoas estejam trabalhando ao mesmo tempo
+#git pool .
